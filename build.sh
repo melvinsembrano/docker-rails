9f7ad46f7995a4a400a31ba99ee0ebff3c1ff9f6
Melvin Sembrano <melvinsembrano@gmail.com>
2019-09-18 00:02:07 +1000

    add test and dev images

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@
 docker build -f ${1}/Dockerfile.base -t melvinsembrano/rails:${1}-base $1/
 set -ex
 docker build -f ${1}/Dockerfile.dev -t melvinsembrano/rails:${1}-dev --build-arg BASE_VERSION=${1} $1/
+docker build -f ${1}/Dockerfile.test -t melvinsembrano/rails:${1}-test --build-arg BASE_VERSION=${1} $1/
 
-# docker push melvinsembrano/rails:${1}-base
+docker push melvinsembrano/rails:${1}-base
+docker push melvinsembrano/rails:${1}-dev
+docker push melvinsembrano/rails:${1}-test
